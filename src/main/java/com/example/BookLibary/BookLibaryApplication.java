2340c09eb50d4b90c2d0776015df529849911961
dz443 <alisealka@pearlgroup.no>
2022-06-20 16:08:40 +0300

    - add files to VCS

---
+++ src/main/java/com/example/BookLibary/BookLibaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookLibary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookLibary/BookLibaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookLibary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
